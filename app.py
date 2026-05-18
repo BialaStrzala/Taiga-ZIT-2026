cbb8f43c72985141e052cf8d95a686169f351278
krlpopiel <admir0305@gmail.com>
2026-05-18 15:21:00 +0200

    TG-10#Done

--- app.py
+++ app.py
@@ -3,3 +3,11 @@
 #=============================================================
 
 # Tu wklej kod do swojego zadania 
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def strona_glowna():
+    return render_template('index.html')
